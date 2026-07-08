a2effbae19f33a534e82313cb5bd6dfc4fcb4d79
shinsujae <wohbin7016@gmail.com>
2026-07-08 19:43:36 +0900

    대시보드 갱신 2026-07-08 19:43

--- js/data.js
+++ js/data.js
@@ -8,15 +8,15 @@ window.DATA = {
       "channel_id": "UCO6p7TPmjQ1lqC0PjW9F0Ag",
       "subscribers": 31700,
       "sampled": 20,
-      "avg_views": 1936,
-      "median_views": 1514,
+      "avg_views": 1904,
+      "median_views": 1489,
       "uploads_30d": 20,
-      "views_30d": 38731,
+      "views_30d": 38094,
       "shorts_ratio": 1.0,
       "top_videos": [
         {
           "title": "발만 넣으면 3초, 신발에 무슨 일이 벌어질까",
-          "views": 7178,
+          "views": 7188,
           "date": "2026-07-02",
           "duration": 18,
           "url": "https://www.youtube.com/watch?v=4wsATmn3iCI"
@@ -30,58 +30,65 @@ window.DATA = {
         },
         {
           "title": "빨래에서 꿉꿉한 냄새? 세제 아무리 바꿔도 소용없던 진짜 이유",
-          "views": 2284,
+          "views": 2288,
           "date": "2026-07-07",
           "duration": 24,
           "url": "https://www.youtube.com/watch?v=fnxn2bm3lo8"
         },
         {
           "title": "밑창 떨어진 신발 버리지 마세요 5분이면 새것 됩니다",
-          "views": 2162,
+          "views": 2165,
           "date": "2026-06-30",
           "duration": 21,
           "url": "https://www.youtube.com/watch?v=AvA8_H-lyUk"
         },
         {
-          "title": "막힌 샤워헤드 식초로 뚫기",
-          "views": 2093,
-          "date": "2026-06-24",
-          "duration": 20,
-          "url": "https://www.youtube.com/watch?v=9zQYmauGwoM"
+          "title": "돌처럼 굳은 페인트 붓, 식초로 복원",
+          "views": 2073,
+          "date": "2026-06-25",
+          "duration": 16,
+          "url": "https://www.youtube.com/watch?v=wFD7re2ml6U"
         }
       ],
       "recent_videos": [
         {
+          "title": "천장에서 물 뚝뚝, 지붕은 멀쩡한데… 진짜 범인은 이 나사입니다",
+          "views": 791,
+          "date": "2026-07-08",
+          "duration": 35,
+          "url": "https://www.youtube.com/watch?v=LUkTQ_7HXCg"
+        },
+        {
           "title": "전자레인지 찌든때, 세제로 문지르지 마세요",
-          "views": 553,
+          "views": 1133,
           "date": "2026-07-08",
           "duration": 24,
           "url": "https://www.youtube.com/watch?v=F4RmYWBYlJk"
         },
         {
           "title": "빨래에서 꿉꿉한 냄새? 세제 아무리 바꿔도 소용없던 진짜 이유",
-          "views": 2284,
+          "views": 2288,
           "date": "2026-07-07",
           "duration": 24,
           "url": "https://www.youtube.com/watch?v=fnxn2bm3lo8"
         },
         {
           "title": "다림질만 하면 옷에 갈색 얼룩… 다리미 고장이 아니었어요",
-          "views": 1373,
+          "views": 1377,
           "date": "2026-07-07",
           "duration": 33,
           "url": "https://www.youtube.com/watch?v=2vLPpIj684Q"
         },
         {
           "title": "수압 약해진 진짜 이유, 수도관이 아니었어요",
-          "views": 1507,
+          "views": 1563,
           "date": "2026-07-06",
           "duration": 33,
           "url": "https://www.youtube.com/watch?v=ZTaLdgT3DLw"
         },
         {
           "title": "가구에 파인 깊은 긁힘, 호두 하나 문질렀더니",
-          "views": 1949,
+          "views": 1952,
           "date": "2026-07-05",
           "duration": 27,
           "url": "https://www.youtube.com/watch?v=pNbxtVfYYXM"
@@ -109,20 +116,13 @@ window.DATA = {
         },
         {
           "title": "발만 넣으면 3초, 신발에 무슨 일이 벌어질까",
-          "views": 7178,
+          "views": 7188,
           "date": "2026-07-02",
           "duration": 18,
           "url": "https://www.youtube.com/watch?v=4wsATmn3iCI"
-        },
-        {
-          "title": "열쇠도 없이 자물쇠가 스르륵, 이 작은 공구의 정체 #생활꿀팁 #꿀팁",
-          "views": 1177,
-          "date": "2026-07-02",
-          "duration": 19,
-          "url": "https://www.youtube.com/watch?v=9IAhdi_UIWM"
         }
       ],
-      "analyzed_at": "2026-07-08 15:56",
+      "analyzed_at": "2026-07-08 19:41",
       "subs_delta": 0
     },
     {
@@ -131,22 +131,29 @@ window.DATA = {
       "channel_id": "UC_yso506oAtYKFyyS77pzLw",
       "subscribers": 72900,
       "sampled": 20,
-      "avg_views": 1132,
+      "avg_views": 1158,
       "median_views": 1429,
       "uploads_30d": 20,
-      "views_30d": 22651,
+      "views_30d": 23179,
       "shorts_ratio": 1.0,
       "top_videos": [
         {
           "title": "자취방에 쌓이던 손빨래가 통째로 사라진 진짜 이유",
-          "views": 2113,
+          "views": 2120,
           "date": "2026-07-07",
           "duration": 29,
           "url": "https://www.youtube.com/watch?v=UbwOUwGpHBc"
         },
         {
+          "title": "와인 코르크 부러뜨려 창피했던 내가, 버튼 하나로 3초 만에 딴 이유",
+          "views": 1982,
+          "date": "2026-07-08",
+          "duration": 30,
+          "url": "https://www.youtube.com/watch?v=msWPyr9PTm4"
+        },
+        {
           "title": "발뒤꿈치에 대기만 했는데 굳은살이 우수수, 만원대 이거",
-          "views": 1837,
+          "views": 1838,
           "date": "2026-07-03",
           "duration": 43,
           "url": "https://www.youtube.com/watch?v=mwW7lKcWCfc"
@@ -164,40 +171,33 @@ window.DATA = {
           "date": "2026-07-04",
           "duration": 34,
           "url": "https://www.youtube.com/watch?v=qUCPoh--hXs"
-        },
-        {
-          "title": "매일 화장실 청소로 팔 빠지던 내가, 만원짜리 이거 하나에 수세미 버린 이유",
-          "views": 1607,
-          "date": "2026-07-07",
-          "duration": 45,
-          "url": "https://www.youtube.com/watch?v=kB__cVQ2P_U"
         }
       ],
       "recent_videos": [
         {
           "title": "와인 코르크 부러뜨려 창피했던 내가, 버튼 하나로 3초 만에 딴 이유",
-          "views": 1468,
+          "views": 1982,
           "date": "2026-07-08",
           "duration": 30,
           "url": "https://www.youtube.com/watch?v=msWPyr9PTm4"
         },
         {
           "title": "자취방에 쌓이던 손빨래가 통째로 사라진 진짜 이유",
-          "views": 2113,
+          "views": 2120,
           "date": "2026-07-07",
           "duration": 29,
           "url": "https://www.youtube.com/watch?v=UbwOUwGpHBc"
         },
         {
           "title": "매일 화장실 청소로 팔 빠지던 내가, 만원짜리 이거 하나에 수세미 버린 이유",
-          "views": 1607,
+          "views": 1612,
           "date": "2026-07-07",
           "duration": 45,
           "url": "https://www.youtube.com/watch?v=kB__cVQ2P_U"
         },
         {
           "title": "버리려던 니트, 다시 꺼내 입게 만든 만원짜리의 정체",
-          "views": 509,
+          "views": 510,
           "date": "2026-07-06",
           "duration": 27,
           "url": "https://www.youtube.com/watch?v=lZC1kWFkN1o"
@@ -225,7 +225,7 @@ window.DATA = {
         },
         {
           "title": "발뒤꿈치에 대기만 했는데 굳은살이 우수수, 만원대 이거",
-          "views": 1837,
+          "views": 1838,
           "date": "2026-07-03",
           "duration": 43,
           "url": "https://www.youtube.com/watch?v=mwW7lKcWCfc"
@@ -245,7 +245,7 @@ window.DATA = {
           "url": "https://www.youtube.com/watch?v=RmYNrGYJahM"
         }
       ],
-      "analyzed_at": "2026-07-08 15:57",
+      "analyzed_at": "2026-07-08 19:42",
       "subs_delta": 0
     },
     {
@@ -254,10 +254,10 @@ window.DATA = {
       "channel_id": "UCC7ZtDMOdJU80HF30wMWM3Q",
       "subscribers": 2370,
       "sampled": 20,
-      "avg_views": 1300,
-      "median_views": 1334,
+      "avg_views": 1344,
+      "median_views": 1335,
       "uploads_30d": 20,
-      "views_30d": 26000,
+      "views_30d": 26897,
       "shorts_ratio": 1.0,
       "top_videos": [
         {
@@ -298,22 +298,29 @@ window.DATA = {
       ],
       "recent_videos": [
         {
-          "title": "이 거미의 정체는? 새를 잡아먹는 뱀",
+          "title": "곰도 사냥해 잡아먹는 이 동물의 정체는?",
           "views": 899,
+          "date": "2026-07-08",
+          "duration": 44,
+          "url": "https://www.youtube.com/watch?v=iza1oSpwYvM"
+        },
+        {
+          "title": "이 거미의 정체는? 새를 잡아먹는 뱀",
+          "views": 1205,
           "date": "2026-07-08",
           "duration": 27,
           "url": "https://www.youtube.com/watch?v=ZGhfgQ_dRzk"
         },
         {
           "title": "실버백 고릴라 vs 사자, 진짜 누가 이길까?",
-          "views": 1373,
+          "views": 1391,
           "date": "2026-07-07",
           "duration": 35,
           "url": "https://www.youtube.com/watch?v=xNpdHStHc_w"
         },
         {
           "title": "사람을 죽일 수 있는 새, 세상에서 가장 위험한 새의 정체",
-          "views": 1255,
+          "views": 1256,
           "date": "2026-07-07",
           "duration": 28,
           "url": "https://www.youtube.com/watch?v=af3zzEFpdWc"
@@ -341,7 +348,7 @@ window.DATA = {
         },
         {
           "title": "캥거루가 아무리 해도 뒤로는 못 걷는 진짜 이유",
-          "views": 1329,
+          "views": 1331,
           "date": "2026-07-03",
           "duration": 24,
           "url": "https://www.youtube.com/watch?v=cF2thcdtYCw"
@@ -359,16 +366,9 @@ window.DATA = {
           "date": "2026-07-01",
           "duration": 26,
           "url": "https://www.youtube.com/watch?v=bLxhd7IQfCo"
-        },
-        {
-          "title": "세상에서 가장 겁 없는 동물이 코브라에 물려도 멀쩡한 이유",
-          "views": 1501,
-          "date": "2026-07-01",
-          "duration": 23,
-          "url": "https://www.youtube.com/watch?v=145jVCEOpMs"
         }
       ],
-      "analyzed_at": "2026-07-08 15:57",
+      "analyzed_at": "2026-07-08 19:42",
       "subs_delta": 0
     },
     {
@@ -377,43 +377,43 @@ window.DATA = {
       "channel_id": "UC29M9AGVR19NtIrAHsjx-XA",
       "subscribers": 1160,
       "sampled": 20,
-      "avg_views": 4910,
-      "median_views": 1324,
+      "avg_views": 4987,
+      "median_views": 1376,
       "uploads_30d": 20,
-      "views_30d": 98200,
+      "views_30d": 99745,
       "shorts_ratio": 1.0,
       "top_videos": [
         {
           "title": "혼다 시티 방지턱만 넘으면 통통 튀는 진짜 원인은 앞쇼바",
-          "views": 47960,
+          "views": 48769,
           "date": "2026-07-02",
           "duration": 26,
           "url": "https://www.youtube.com/watch?v=rL9baE8p6kA"
         },
         {
           "title": "현대 i10 시동 약함, 진짜 원인은 발전기(알터네이터)",
-          "views": 19051,
+          "views": 19083,
           "date": "2026-06-25",
           "duration": 26,
           "url": "https://www.youtube.com/watch?v=NqLSWQtSIRE"
         },
         {
           "title": "아우디 RS Q8 엠블럼, 드라이버로 뜯으면 망합니다",
-          "views": 8289,
+          "views": 8293,
           "date": "2026-06-28",
           "duration": 18,
           "url": "https://www.youtube.com/watch?v=gtCZEXllbRQ"
         },
         {
           "title": "냉각수 아무리 채워도 온도가 계속 레드존, 진짜 원인은?",
-          "views": 7482,
+          "views": 7635,
           "date": "2026-07-04",
           "duration": 30,
           "url": "https://www.youtube.com/watch?v=lwAcmZSoGPU"
         },
         {
           "title": "BMW E90 주차하면 타는 냄새, 진짜 원인은 밸브커버 가스켓",
-          "views": 3071,
+          "views": 3081,
           "date": "2026-06-29",
           "duration": 21,
           "url": "https://www.youtube.com/watch?v=RmXO-tBVOjo"
@@ -421,79 +421,79 @@ window.DATA = {
       ],
       "recent_videos": [
         {
+          "title": "혼다 시티 신호만 걸리면 부르르 시동 꺼질 듯, 배터리 아니라 스로틀바디였다",
+          "views": 6,
+          "date": "2026-07-08",
+          "duration": 29,
+          "url": "https://www.youtube.com/watch?v=-4Get_WrrwI"
+        },
+        {
           "title": "BMW 변속할 때 울컥울컥, 미션 고장인 줄 알았는데 진짜 원인은",
-          "views": 372,
+          "views": 666,
           "date": "2026-07-08",
           "duration": 34,
           "url": "https://www.youtube.com/watch?v=bcDYD4Pymy0"
         },
         {
           "title": "토요타 갑자기 기름 먹는 하마, 미션 아니고 진짜 원인은 카본 낀 에어플로우 센서",
-          "views": 69,
+          "views": 118,
           "date": "2026-07-07",
           "duration": 35,
           "url": "https://www.youtube.com/watch?v=nqfZ937zZT8"
         },
         {
           "title": "BMW 변속할 때 울컥 충격, 미션 갈 뻔했는데 진짜 원인은 '평생 무교환' 미션오일",
-          "views": 1306,
+          "views": 1410,
           "date": "2026-07-07",
           "duration": 36,
           "url": "https://www.youtube.com/watch?v=te6KfTQcCc4"
         },
         {
           "title": "토요타 타코마, 브레이크만 밟으면 핸들이 부들부들… 타이어 갈아도 안 잡히는 진짜 원인",
-          "views": 1258,
+          "views": 1310,
           "date": "2026-07-06",
           "duration": 26,
           "url": "https://www.youtube.com/watch?v=AUBA1SyV4Os"
         },
         {
           "title": "폭스바겐 골프 뒷브레이크, 그냥 밀어넣으면 캘리퍼 망가집니다",
-          "views": 2332,
+          "views": 2399,
           "date": "2026-07-05",
           "duration": 29,
           "url": "https://www.youtube.com/watch?v=KKiYhFhEQzE"
         },
         {
           "title": "냉각수 아무리 채워도 온도가 계속 레드존, 진짜 원인은?",
-          "views": 7482,
+          "views": 7635,
           "date": "2026-07-04",
           "duration": 30,
           "url": "https://www.youtube.com/watch?v=lwAcmZSoGPU"
         },
         {
           "title": "엘란트라 핸들 꺾을 때만 딱딱딱, 대체 뭐가 문제일까",
-          "views": 70,
+          "views": 72,
           "date": "2026-07-03",
           "duration": 30,
           "url": "https://www.youtube.com/watch?v=nCoLYWlwAtQ"
         },
         {
           "title": "차 바닥이 흥건한데 창문은 멀쩡? 진짜 범인은 여기였다",
-          "views": 1774,
+          "views": 1775,
           "date": "2026-07-03",
           "duration": 30,
           "url": "https://www.youtube.com/watch?v=VtjFL6suLso"
         },
         {
           "title": "혼다 시티 방지턱만 넘으면 통통 튀는 진짜 원인은 앞쇼바",
-          "views": 47960,
+          "views": 48769,
           "date": "2026-07-02",
           "duration": 26,
           "url": "https://www.youtube.com/watch?v=rL9baE8p6kA"
-        },
-        {
-          "title": "밟을 때마다 차가 울컥, 미션 고장인 줄 알았는데 진짜 범인은 점화코일",
-          "views": 79,
-          "date": "2026-07-01",
-          "duration": 26,
-          "url": "https://www.youtube.com/watch?v=VnKxOtvnEE0"
         }
       ],
-      "analyzed_at": "2026-07-08 15:58",
+      "analyzed_at": "2026-07-08 19:43",
       "subs_delta": 0
     }
   ],
-  "generated_at": "2026-07-08 15:56"
+  "generated_at": "2026-07-08 19:41"
 };
