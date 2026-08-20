4bf27eca606dcf17c4edcec5f2c1a8854165e187
shinsujae <wohbin7016@gmail.com>
2026-08-21 07:17:18 +0900

    대시보드 갱신 2026-08-21 07:17

--- js/data.js
+++ js/data.js
@@ -369,7 +369,7 @@ window.DATA = {
         }
       ],
       "analyzed_at": "2026-08-20 21:44",
-      "subs_delta": 110
+      "subs_delta": 0
     },
     {
       "url": "https://youtube.com/@viewpoint-q9y",
@@ -492,7 +492,7 @@ window.DATA = {
         }
       ],
       "analyzed_at": "2026-08-20 21:45",
-      "subs_delta": 20
+      "subs_delta": 0
     },
     {
       "url": "https://www.youtube.com/@해외반응픽",
@@ -615,7 +615,7 @@ window.DATA = {
         }
       ],
       "analyzed_at": "2026-08-20 21:46",
-      "subs_delta": 2
+      "subs_delta": 0
     },
     {
       "url": "https://www.youtube.com/channel/UChq2TqKlpNlWpHpDRMwyCeQ",
@@ -731,7 +731,7 @@ window.DATA = {
         }
       ],
       "analyzed_at": "2026-08-20 21:46",
-      "subs_delta": 2
+      "subs_delta": 0
     }
   ],
   "generated_at": "2026-08-20 21:43",
